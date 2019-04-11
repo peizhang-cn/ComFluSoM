cd8f8373235c929ac84debcc569c73618ff40d3e
PeiZhang <peizhang.hhu@gmail.com>
2019-04-11 19:02:06 +1000

    fix header.h path

--- Library/MPM/MPM.h
+++ Library/MPM/MPM.h
@@ -17,7 +17,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>  *
  ************************************************************************/
 
-#include "./HEADER.h"
+#include "../HEADER.h"
 #include <SHAPE.h>
 #include <MPM_PARTICLE.h>
 
