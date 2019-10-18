56217a5c18c0df939388e9023a11857f10dcab09
PeiZhang <peizhang.hhu@gmail.com>
2019-10-18 16:24:56 +1000

    add header

--- Library/HEADER.h
+++ Library/HEADER.h
@@ -7,8 +7,11 @@
 #include <cmath>
 #include <algorithm>
 #include <string.h>
+#include <unistd.h>
 #include <stdexcept>
 #include <chrono>
+#include <unordered_map>
+// #include <set>
 #include <H5Cpp.h>
 #include <Eigen/Dense>
 #include <Eigen/QR>
